9264cdbfd9ede2cf76651fbeefc64d280e9a2375
guillem.cordoba <guillem.cordoba@gmail.com>
2025-01-10 10:53:28 +0100

    Updated flake

--- tests/src/setup.ts
+++ tests/src/setup.ts
@@ -11,8 +11,6 @@ import { ProfilesClient } from '../../ui/src/profiles-client.js';
 import { ProfilesStore } from '../../ui/src/profiles-store.js';
 
 export async function setup(scenario: Scenario) {
-	scenario.dpkiNetworkSeed = undefined;
-
 	const testHappUrl =
 		dirname(fileURLToPath(import.meta.url)) +
 		'/../../workdir/profiles-test.happ';
