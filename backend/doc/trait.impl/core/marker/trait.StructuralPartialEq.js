67a2db3cd0c2ed53033e725aa62d21dbaa72995f
guillemcordoba <guillemcordoba@users.noreply.github.com>
2025-07-02 11:26:00 +0000

    Deploying to gh-pages from @ darksoil-studio/profiles-zome@1763ce23c02168cf3c76b1e3e13f53f6366afd17 🚀

--- backend/doc/trait.impl/core/marker/trait.StructuralPartialEq.js
+++ backend/doc/trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["profiles_integrity",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"profiles_integrity/enum.LinkTypes.html\" title=\"enum profiles_integrity::LinkTypes\">LinkTypes</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"profiles_integrity/enum.UnitEntryTypes.html\" title=\"enum profiles_integrity::UnitEntryTypes\">UnitEntryTypes</a>"]]]]);
+    var implementors = Object.fromEntries([["profiles_integrity",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"profiles_integrity/enum.LinkTypes.html\" title=\"enum profiles_integrity::LinkTypes\">LinkTypes</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"profiles_integrity/enum.UnitEntryTypes.html\" title=\"enum profiles_integrity::UnitEntryTypes\">UnitEntryTypes</a>"]]],["profiles_provider_zome_trait",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"profiles_provider_zome_trait/struct.Profile.html\" title=\"struct profiles_provider_zome_trait::Profile\">Profile</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[671]}
+//{"start":57,"fragment_lengths":[671,371]}
